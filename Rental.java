74f2a1af75c72d895ff919114f1587558b629bf7
Larissa Aline <53979236+larissafv@users.noreply.github.com>
2022-02-04 20:28:36 -0300

    Commit 4
    Commit 4

--- Rental.java
+++ Rental.java
@@ -15,4 +15,23 @@ public class Rental {
    public Movie getMovie() {
       return _movie;
    }
+
+   public double getCharge() {
+        //determine amounts for each line
+        switch (each.getMovie().getPriceCode()) {
+        case Movie.REGULAR:
+            thisAmount += 2;
+            if (each.getDaysRented() > 2)
+                thisAmount += (each.getDaysRented() - 2) * 1.5;
+            break;
+        case Movie.NEW_RELEASE:
+            thisAmount += each.getDaysRented() * 3;
+            break;
+        case Movie.CHILDRENS:
+            thisAmount += 1.5;
+            if (each.getDaysRented() > 3)
+                thisAmount += (each.getDaysRented() - 3) * 1.5;
+            break;
+        }
+   }
 }
