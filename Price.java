7bc2f2652fa66de79d4bc5d7ca342469fd0ff9d7
Larissa Aline <53979236+larissafv@users.noreply.github.com>
2022-02-04 22:05:20 -0300

    Commit 15
    Commit 15

--- Price.java
+++ Price.java
@@ -4,7 +4,6 @@ public abstract class Price {
     public abstract double getCharge(int daysRented);
 
     public int getFrequentRenterPoints(int daysRented) {
-        // add frequent renter points
-        frequentRenterPoints ++;
+        return 1;
     }
 }
