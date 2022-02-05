4b2d0e7f90d18ed6ab56636cd22fd83c9d5f953d
Larissa Aline <53979236+larissafv@users.noreply.github.com>
2022-02-04 21:19:20 -0300

    Commit 9
    Commit 9

--- Movie.java
+++ Movie.java
@@ -1,26 +1,45 @@
 public class Movie {
 
-  public static final int  CHILDRENS = 2;
-  public static final int  REGULAR = 0;
-  public static final int  NEW_RELEASE = 1;
+    public static final int  CHILDRENS = 2;
+    public static final int  REGULAR = 0;
+    public static final int  NEW_RELEASE = 1;
 
-  private String _title;
-  private int _priceCode;
+    private String _title;
+    private int _priceCode;
 
-  public Movie(String title, int priceCode) {
-      _title = title;
-      _priceCode = priceCode;
-  }
+    public Movie(String title, int priceCode) {
+        _title = title;
+        _priceCode = priceCode;
+    }
 
-  public int getPriceCode() {
-      return _priceCode;
-  }
+    public int getPriceCode() {
+        return _priceCode;
+    }
 
-  public void setPriceCode(int arg) {
-      _priceCode = arg;
-  }
+    public void setPriceCode(int arg) {
+        _priceCode = arg;
+    }
 
-  public String getTitle (){
-      return _title;
-  }
+    public String getTitle (){
+        return _title;
+    }
+
+    public double getCharge(int daysRented){
+        //determine amounts for each line
+        switch (each.getMovie().getPriceCode()) {
+        case Movie.REGULAR:
+            thisAmount += 2;
+            if (each.getDaysRented() > 2)
+                thisAmount += (each.getDaysRented() - 2) * 1.5;
+            break;
+        case Movie.NEW_RELEASE:
+            thisAmount += each.getDaysRented() * 3;
+            break;
+        case Movie.CHILDRENS:
+            thisAmount += 1.5;
+            if (each.getDaysRented() > 3)
+                thisAmount += (each.getDaysRented() - 3) * 1.5;
+            break;
+        }
+    }
 }
--- Rental.java
+++ Rental.java
@@ -1,42 +1,27 @@
 public class Rental {
 
-   private Movie _movie;
-   private int _daysRented;
+    private Movie _movie;
+    private int _daysRented;
 
-   public Rental(Movie movie, int daysRented) {
-      _movie = movie;
-      _daysRented = daysRented;
-   }
+    public Rental(Movie movie, int daysRented) {
+        _movie = movie;
+        _daysRented = daysRented;
+    }
 
-   public int getDaysRented() {
-      return _daysRented;
-   }
+    public int getDaysRented() {
+        return _daysRented;
+    }
 
-   public Movie getMovie() {
-      return _movie;
-   }
+    public Movie getMovie() {
+        return _movie;
+    }
 
-   public double getCharge() {
-        //determine amounts for each line
-        switch (each.getMovie().getPriceCode()) {
-        case Movie.REGULAR:
-            thisAmount += 2;
-            if (each.getDaysRented() > 2)
-                thisAmount += (each.getDaysRented() - 2) * 1.5;
-            break;
-        case Movie.NEW_RELEASE:
-            thisAmount += each.getDaysRented() * 3;
-            break;
-        case Movie.CHILDRENS:
-            thisAmount += 1.5;
-            if (each.getDaysRented() > 3)
-                thisAmount += (each.getDaysRented() - 3) * 1.5;
-            break;
-        }
-   }
+    public double getCharge() {
+        return _movie.getCharge(_daysRented);
+    }
 
-   public int getFrequentRenterPoints() { 
+    public int getFrequentRenterPoints() { 
         // add frequent renter points
         frequentRenterPoints ++;
-   }
+    }
 }
